09f164fd83e72047ce5a9c3a489b1c1300030b1d
Андрей Васильев <vasilyev.an@niistt.ru>
2024-07-26 18:00:50 +0300

    Filesystem using, get positions dev

--- TextAnalyzer/Source.cpp
+++ TextAnalyzer/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 #include <vector> 
 #include <string>
@@ -9,13 +10,19 @@
 #include <chrono>
 #include <future>
 #include <queue>
+#include <tuple>
 
-#define THREADS_DEFAULT 4
+constexpr auto THREADS_DEFAULT = 4;
+constexpr auto FILENAME_DEFAULT = "file.txt";
 
+namespace fs = std::filesystem;
 using namespace std;
 
 class text_analyzer
 {
+
+	using position = pair<int, int>;
+
 	struct compare
 	{
 		bool operator()(const int& a, const int& b)
@@ -24,42 +31,62 @@ class text_analyzer
 		}
 	};
 
+	struct position_compare
+	{
+		bool operator()(const position& a, const position& b)
+		{
+			return a.first >= b.first && a.second >= b.second;
+		}
+	};
+
 	using indexes_array = priority_queue<int, vector<int>, compare>;
+	using positions_array = vector<position>;
 
 public:
 
-	indexes_array get_indexes() const
+	text_analyzer() {};
+
+	text_analyzer(const string_view _file_path) : text_analyzer()
 	{
-		return indexes;
+		file_path = _file_path;
 	}
 
-	int find_substr(const string filename, const string substr, const size_t threads_count = 1)
+	indexes_array get_indexes() const
 	{
-		std::ifstream f(filename, ios::binary);
-		if (!f.is_open() || f.eof())
+		return substr_indexes;
+	}
+
+	static positions_array get_positions(indexes_array indexes)
+	{
+		positions_array substr_positions{};
+		
+		return substr_positions;
+	}
+
+	int find_substr(const string substr, const size_t threads_count = 1)
+	{
+		if (!filesystem::exists(file_path) || filesystem::file_size(file_path) == 0)
 			return -1;
 
 		if (substr.size() == 0)
 			return 0;
 
-		atomic<int> count = 0;
+		filesystem::path filesystem_path(file_path);
 
-		f.seekg(0, std::ios::end);
-		int text_size = f.tellg();
+		auto text_size = filesystem::file_size(file_path);
 		cout << format("Text size - {} bytes\n", text_size);
 
 		vector < future<int>> threads(threads_count);
 
-		int rem = text_size % threads_count;
-		int string_size = text_size / threads_count;
-		int pos = 0;
+		size_t rem = text_size % threads_count;
+		size_t string_size = text_size / threads_count;
+		size_t pos = 0;
 
-		//flag_write.test_and_set();
-		//flag_write.notify_one();
+		atomic<int> count = 0;
 
 		for (auto& my_thread : threads)
 		{
-			my_thread = async(launch::async, &text_analyzer::_find_substr, this, filename, substr, pos, string_size + (rem > 0 ? 1 : 0));
+			my_thread = async(launch::async, &text_analyzer::_find_substr, this, substr, pos, string_size + (rem > 0 ? 1 : 0));
 			pos += string_size + (rem > 0 ? 1 : 0);
 			if (rem > 0)
 				rem--;
@@ -75,16 +102,16 @@ public:
 
 private:
 
-	int _find_substr(const string_view filename, const string_view substr, const int offset = 0, const int bytes_to_read = 0)
+	int _find_substr(const string_view substr, const size_t offset = 0, const size_t bytes_to_read = 0)
 	{
-		auto begin = chrono::system_clock::now();
+		//auto begin = chrono::system_clock::now();
 
 		int thread_id = thread_count.load();
 		thread_count.fetch_add(1);
 
 		//cout << format("Thread {} from pos {} - {} bytes\n", thread_id, offset, bytes_to_read);
 
-		ifstream file(string(filename).c_str(), ios::binary);
+		ifstream file(file_path.native(), ios::binary);
 		file.seekg(offset, ios_base::beg);
 
 		size_t substr_size = substr.size();
@@ -103,7 +130,8 @@ private:
 			{
 
 				while (flag_write.test_and_set(memory_order_acquire)) {}
-				indexes.push(offset + i - substr_size);
+				substr_indexes.push(int(offset + i - substr_size));
+				
 				flag_write.clear(memory_order_release);				
 
 				count.fetch_add(1);
@@ -116,19 +144,39 @@ private:
 				symbols_found = 0;
 		}
 
-		auto end = chrono::system_clock::now();
+		//auto end = chrono::system_clock::now();
 
 		//cout << format("Symbol after last thread {} - {}", thread_id, (char)file.peek()) << endl;
 
 		file.close();
 
-		cout << format("Thread {} time - {} mcs\n", thread_id, chrono::duration_cast<chrono::microseconds>(end - begin).count());
+		//cout << format("Thread {} time - {} mcs\n", thread_id, chrono::duration_cast<chrono::microseconds>(end - begin).count());
 
 		return count;
 	}
 
-	indexes_array indexes{};
+	vector<int> get_line_feeds()
+	{
+		vector<int> line_feeds_array{};
+		ifstream file(file_path.native(), ios::binary);
+		int count = 0;
+		while (!file.eof())
+		{
+			if (file.get() == '\n')
+				line_feeds_array.push_back(count);
+
+			count++;
+		}
+
+
+	}
+
+
+	indexes_array substr_indexes{};
+	positions_array* substr_positions = nullptr;
 	inline static atomic<int> thread_count = 1;
+
+	fs::path file_path;
 
 	atomic_flag flag_write = ATOMIC_FLAG_INIT;
 };
@@ -136,21 +184,26 @@ private:
 
 int main(int argc, char** argv)
 {
-	string filename{};
+	fs::path file_path{};
 	if (argc >= 2)
-		filename = argv[1];
+		file_path = argv[1];
 	else
 	{
 		string_view path_to_exe = argv[0];
-		string path_to_folder = string(argv[0]).substr(0, path_to_exe.find_last_of('\\') + 1);
-		filename = path_to_folder + "file.txt";
+		auto path_to_folder = filesystem::path(path_to_exe).parent_path();
+		for (const auto& p : fs::recursive_directory_iterator(path_to_folder))
+			if (p.path().filename() == FILENAME_DEFAULT)
+			{
+				file_path = p.path();
+				break;
+			}
 	}
-	if (!fstream(filename).is_open())
+	if (file_path == " " || !filesystem::exists(file_path))
 	{
 		cout << "Invalid file path" << endl;
 		return -1;
 	}
-	cout << format("Path to .txt file - {}", filename) << endl;
+	cout << format("Path to .txt file - {}", file_path.string()) << endl;
 	string substr{};
 	if (argc >= 3)
 	{
@@ -169,14 +222,14 @@ int main(int argc, char** argv)
 		threads_count = THREADS_DEFAULT;
 
 
-	text_analyzer text1;
+	text_analyzer text1(file_path.string());
 
 	cout << format("Threads count - {}", threads_count) << endl;
 	cout << format("String to find - {}", substr) << endl;
 
 	auto begin = chrono::system_clock::now();
 
-	cout << format("Indexes found - {}", text1.find_substr(filename, substr, threads_count)) << endl;
+	cout << format("Indexes found - {}", text1.find_substr(substr, threads_count)) << endl;
 
 	auto end = chrono::system_clock::now();
 
